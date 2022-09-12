370123327ef580a7e606d3f5fb575c18dab1cd26
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-09-12 15:37:09 +0500

    INITIAL COMMIT

---
+++ src/app/feature/feature.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureRoutingModule } from './feature-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureRoutingModule
+  ]
+})
+export class FeatureModule { }
